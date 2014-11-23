7dbbd2334466432b764c0c0d3dfed5bd5f9e5e21
Albert Chu <achu@coverity.com>
2014-11-22 17:40:02 -0700

    Added sessionWorthPair object try 2

---
+++ src/examSchedule/assignment/SessionWorthPair.java
@@ -0,0 +1,30 @@
+package examSchedule.assignment;
+
+public class SessionWorthPair implements Comparable<SessionWorthPair>
+{
+	private Integer worth;
+	private Session session;
+	
+	public SessionWorthPair(Session session, int worth)
+	{
+		this.session = session;
+		this.worth = worth;
+	}
+	
+	public int getWorth()
+	{
+		return worth;
+	}
+
+	public Session getSession()
+	{
+		return session;
+	}
+
+	@Override
+	public int compareTo(SessionWorthPair o)
+	{
+		return this.worth.compareTo(o.worth);
+	}
+
+}
